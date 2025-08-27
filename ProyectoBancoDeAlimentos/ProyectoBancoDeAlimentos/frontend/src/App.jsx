3420d57587ce905f89be4a9de26c9b9339a810c3
AbrahamReyes08 <124213092+AbrahamReyes08@users.noreply.github.com>
2025-08-27 09:07:32 -0600

    correcion

--- ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/frontend/src/App.jsx
+++ ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/frontend/src/App.jsx
@@ -1,14 +1,5 @@
 import "./App.css";
-//import Header from "./components/Header";
-//import Start from "./components/Start";
-//import Error404 from "./components/Error404";
-//<Route path="/" element={<Menu />} />
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./dashboard.jsx";
-import Login from "./login.jsx";
-import Crear_cuenta from "./crear_cuenta.jsx";
-import Forgot_password from "./forgot_password.jsx";
-import Cambiar_contraseña from "./cambiar_contraseña.jsx";
+
 import Header from "./components/Header";
 import InicioUsuario from "./components/inicioUsuario.jsx";
 
