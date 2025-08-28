bd207f0e573d7e483c53d6873d5e95eaabd291cc
Kenny5231 <124184507+Kenny5231@users.noreply.github.com>
2025-08-27 18:30:01 -0600

    correccion en header

--- ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/frontend/src/App.jsx
+++ ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
-
 import Header from "./components/Header";
-import InicioUsuario from "./components/inicioUsuario.jsx";
-
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./dashboard.jsx";
+import Login from "./login.jsx";
+import Crear_cuenta from "./crear_cuenta.jsx";
+import Forgot_password from "./forgot_password.jsx";
+import Cambiar_contraseña from "./cambiar_contraseña.jsx";
+import InicioUsuario from "./components/inicioUsuario.jsx"
 function App() {
   return (
     <div className="App">
-      <Header />
-      <InicioUsuario />
+      <Header /> {}
+      <Routes>
+        <Route path="/" element={<InicioUsuario />} />
+        <Route path="/crear_cuenta" element={<Crear_cuenta />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot_password" element={<Forgot_password />} />
+        <Route path="/cambiar_contraseña" element={<Cambiar_contraseña />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
