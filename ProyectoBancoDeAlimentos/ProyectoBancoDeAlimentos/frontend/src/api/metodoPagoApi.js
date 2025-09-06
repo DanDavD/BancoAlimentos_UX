4920fecd10892783bc42a29d102c990e02489c4a
alvt1234 <124189136+alvt1234@users.noreply.github.com>
2025-09-05 21:47:23 -0600

    Update metodoPagoApi.js

--- ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/frontend/src/api/metodoPagoApi.js
+++ ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/frontend/src/api/metodoPagoApi.js
@@ -2,17 +2,21 @@ import axiosInstance from "./axiosInstance";
 
 //
 export function getAllMetodoPago() {
-  return axiosInstance.get("/api/metodoPago/");
+  return axiosInstance.get("/api/metodo-pago/");
 }
 
 export function createMetodoPago(payload) {
-  return axiosInstance.post("/api/metodoPago/", payload);
+  return axiosInstance.post("/api/metodo-pago/", payload);
 }
 
 export function updateMetodoPago(id, payload) {
-  return axiosInstance.put(`/api/metodoPago/${id}`, payload);
+  return axiosInstance.put(`/api/metodo-pago/${id}`, payload);
 }
 
 export function deleteMetodoPago(id) {
-  return axiosInstance.delete(`/api/metodoPago/${id}`);
+  return axiosInstance.delete(`/api/metodo-pago/${id}`);
+}
+
+export function setMetodoPagoDefault(id){
+    return axiosInstance.patch(`/api/metodo-pago/default/${id}`);
 }
