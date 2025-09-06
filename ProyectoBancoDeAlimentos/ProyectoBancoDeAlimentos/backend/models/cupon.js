858088047d41758ec4908613924461dbe71969a9
eleanareyesss <eleanareyesss@unitec.edu>
2025-09-06 17:57:49 -0600

    conexion de api de mis cupones

--- ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/backend/models/cupon.js
+++ ProyectoBancoDeAlimentos/ProyectoBancoDeAlimentos/backend/models/cupon.js
@@ -1,19 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-  const cupon = sequelize.define('cupon', {
-    id_cupon:         { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    codigo:           { type: DataTypes.STRING, unique: true },
-    descripcion:      DataTypes.STRING,
-    tipo:             DataTypes.STRING,
-    valor:            DataTypes.DOUBLE,
-    uso_por_usuario:  DataTypes.INTEGER,
-    termina_en:       DataTypes.DATEONLY,
-    activo:           DataTypes.BOOLEAN,
-  }, {
-    tableName: 'cupon',
-    timestamps: false,
-    underscored: true,
-  });
-/*
+  const cupon = sequelize.define(
+    "cupon",
+    {
+      id_cupon: { type: DataTypes.INTEGER, primaryKey: true },
+      tipo: DataTypes.STRING,
+      valor: DataTypes.FLOAT,
+      uso_por_usuario: DataTypes.INTEGER,
+      termina_en: DataTypes.DATE,
+      activo: DataTypes.BOOLEAN,
+      codigo: DataTypes.STRING, // sin tilde
+      descripcion: DataTypes.STRING,
+    },
+    {
+      tableName: "cupon",
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  /*
   cupon.associate = (models) => {
     cupon.hasMany(models.historial_cupon, { foreignKey: 'id_cupon', as: 'historial' });
   };*/
